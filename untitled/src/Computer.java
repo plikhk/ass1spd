4d7f9e42124aaa43241c90b57216b3e403a707cb
plikhk <alikhanmegacoolboy@gmail.com>
2026-09-13 20:21:04 +0500

    setting everything up

--- untitled/src/Computer.java
+++ untitled/src/Computer.java
@@ -1,2 +1,7 @@
 public class Computer {
+    private final String cpu;
+    private final String gpu;
+    private final int ramGb;
+    private final int storageGb;
+    private final String coolingSystem;
 }
