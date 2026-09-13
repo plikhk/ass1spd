de83eec2de7b86999adf2be7d2805a8c7be84cc8
plikhk <alikhanmegacoolboy@gmail.com>
2026-09-13 20:41:23 +0500

    setting everything up

--- untitled/src/Computer.java
+++ untitled/src/Computer.java
@@ -4,4 +4,13 @@ public class Computer {
     private final int ramGb;
     private final int storageGb;
     private final String coolingSystem;
+
+// конструктор протектед, тупо чтоб извне нельзя было через new создать    protected Computer(String cpu, String gpu, int ramGb, int storageGb, String coolingSystem) {
+        this.cpu = cpu;
+        this.gpu = gpu;
+        this.ramGb = ramGb;
+        this.storageGb = storageGb;
+        this.coolingSystem = coolingSystem;
+
+
 }
