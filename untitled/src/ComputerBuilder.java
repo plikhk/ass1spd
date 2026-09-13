4f0fc191989c798b23b1dbdd23cff7014d08e498
plikhk <alikhanmegacoolboy@gmail.com>
2026-09-13 20:51:51 +0500

    setting everything up

--- untitled/src/ComputerBuilder.java
+++ untitled/src/ComputerBuilder.java
@@ -1,6 +1,11 @@
 / абстрактный класс чтоб не копипастить одно и то же в оба билдера (для clean code)
 public abstract class ComputerBuilder{
-
+    // протектед чтобы наследники видели эти поля
+    protected String cpu;
+    protected String gpu;
+    protected int ramGb;
+    protected int storageGb;
+    protected CoolingType cooling = CoolingType.STOCK_AIR; // базовое охлаждение для всех
 
 
 }
